68f1efb4c515a012d9f2040acb409f4ff3323c06
yonemoto <x-na-yonemoto@kddi-tech.com>
2024-10-11 18:11:40 +0900

    Initial Commit

---
+++ WeatherSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherSampleApp
+//
+//  Created by 米本直樹 on 2024/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherSampleApp/WeatherSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSampleAppApp.swift
+//  WeatherSampleApp
+//
+//  Created by 米本直樹 on 2024/10/11.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
